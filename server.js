b0809a40c56d5265118ff1260219f8eaf6c40381
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-06-23 15:08:17 -0400

    added dependencies and connected to DB

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Connect to Mongo
+mongoose
+    .connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to DB...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
