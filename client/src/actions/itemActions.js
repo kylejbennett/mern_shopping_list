f81d55f23f370106e1e9d8806f08b8762458b695
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-06-29 12:11:06 -0400

    added deploy script

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,21 +1,36 @@
-import {DELETE_ITEM, GET_ITEMS, ADD_ITEM} from "./types"
+import axios from 'axios';
+import {DELETE_ITEM, GET_ITEMS, ADD_ITEM, ITEMS_LOADING} from "./types"
 
-export const getItems = () => {
+export const setItemsLoading = () => {
     return{
-        type: GET_ITEMS
+        type: ITEMS_LOADING
     };
 };
 
-export const deleteItem = (id) => {
-    return{
+export const getItems = () => dispatch => {
+    dispatch(setItemsLoading());
+    axios
+        .get('./api/items')
+        .then(res =>
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            })
+        )
+};
+
+export const addItem = (item) => dispatch => {
+    axios.post('./api/items', item)
+        .then(res=> dispatch({
+                type: ADD_ITEM,
+                payload: res.data
+            })
+        )
+};
+
+export const deleteItem = (id) => dispatch => {
+    axios.delete(`./api/items/${id}`).then(res => dispatch({
         type: DELETE_ITEM,
         payload: id
-    };
-};
-
-export const addItem = (item) => {
-    return{
-        type: ADD_ITEM,
-        payload: item
-    };
+    }))
 };
